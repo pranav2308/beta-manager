5e7d6adb1eff183c8fe8d21f24a14a3e08ff5c82
pranav2308 <pranav08@terpmail.umd.edu>
2019-10-11 19:14:58 -0400

    adding authentication cards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+
 import '../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import '../../../node_modules/bootstrap-css-only/css/bootstrap.min.css';
 import '../../../node_modules/mdbreact/dist/css/mdb.css';
+import {MDBIcon} from 'mdbreact';
 import Navbar from '../Navbar/Navbar';
+import Registration from '../Registration/Registration';
+import Login from '../Login/Login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-       <Navbar/>
-    </div>
+  	<div>
+  		<Navbar/>
+		<div className = "authentication">   
+	       <Registration/>
+	    </div>
+	</div>
   );
 }
 
