bccbc53fb20425a046de0120835527370d855086
pranav2308 <pranav08@terpmail.umd.edu>
2019-11-09 22:25:05 -0500

    completing IVS visualization page

--- src/components/Strategies/IVS/IVSVisualize.js
+++ src/components/Strategies/IVS/IVSVisualize.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { VictoryChart, VictoryPie, VictoryTheme, VictoryContainer, VictoryLabel } from 'victory';
+import './IVSVisualize.css';
+
+
+const tableStyle = {
+		marginTop:  '12%',
+  		marginLeft: 'auto',
+  		marginRight : 'auto'
+	}
+const smallHeadingStyle = {
+		color : '#dbd5d0',
+		fontWeight: 'bold',
+		fontSize : '250%'
+	}
 
 class IVSVisualize extends React.Component{
 	
@@ -42,24 +55,71 @@ class IVSVisualize extends React.Component{
 
 
 	render(){
+
+		const data = [
+			{ticker : 'AAPL', allocation : 35},
+			{ticker : 'AVP', allocation : 5},
+			{ticker : 'MRO', allocation : 20},
+			{ticker : 'PACD', allocation : 15},
+			{ticker : 'TSLA', allocation : 25}
+		]
+
+		const tableContent = data.map( (elem, index) => {
+			return (
+					<tr>
+				      <th scope="row">{index + 1}</th>
+				      <td>{elem.ticker}</td>
+				      <td>{elem.allocation}</td>
+				      <td>{100}</td>
+				    </tr>
+				);
+		});
+
 		return (
+			<div className = "ivs-content">
+				<div className = "chart-style">
+					<table style = {tableStyle}>
 
-			<VictoryPie data = {this.state.data} 
-				x = "ticker" 
-				y = "allocation"
-				startAngle={90}
-  				endAngle={450}
-				animate={{ duration: 3000}}
-				innerRadius = {10}
-				labels={({ datum }) => `${datum.ticker}:${datum.allocation.toFixed(2)}%`}
-				labelComponent={<VictoryLabel angle={0} labelPlacement = "perpendicular"/>}
-				labelPosition="centroid"
-				style = {{labels : {fontSize: 8, fill: "white", padding: 15, fontFamily : 'inherit'}}} 
-				padAngle = {2}
-				radius = {75}
-				containerComponent={<VictoryContainer responsive={true} title = {`Title of Pie chart`} desc = {`Description of chart`}/>}
-				colorScale={"qualitative"}/>
+					  <tbody>
+					    <tr>
+					      <td className="align-middle text-center" style = {smallHeadingStyle}>Inverse Variance Portfolio Allocation</td>
+					    </tr>
 
+					  </tbody>
+					</table>
+
+					<VictoryPie data = {this.state.data} 
+						x = "ticker" 
+						y = "allocation"
+						startAngle={90}
+		  				endAngle={450}
+						animate={{ duration: 3000}}
+						innerRadius = {20}
+						labels={({ datum }) => `${datum.ticker}:${datum.allocation.toFixed(2)}%`}
+						labelComponent={<VictoryLabel angle={0} labelPlacement = "perpendicular"/>}
+						labelPosition="centroid"
+						style = {{labels : {fontSize: 8, fill: "white", padding: 15, fontFamily : 'inherit'}}} 
+						padAngle = {2}
+						padding = {70}
+						containerComponent={<VictoryContainer responsive={true} title = {`Title of Pie chart`} desc = {`Description of chart`}/>}
+						colorScale={"qualitative"}/>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-dark table-hover table-striped table-style">
+						<thead className = "table-success table-heading-style">
+						    <tr>
+						      <th scope="col">Sr No.</th>
+						      <th scope="col">Ticker</th>
+						      <th scope="col">Allocation (%)</th>
+						      <th scope="col">Allocaion ($)</th>
+						    </tr>
+					    </thead>
+					    <tbody className = "table-body-style">
+					    	{tableContent}
+					    </tbody>
+  					</table>
+				</div>
+			</div>
 		);	
 	}
 	
